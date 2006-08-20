82915c7ee7501e462ce4cfc568e4183c88ffce5c
Rafael Mizrahi <rafim@mono-cvs.ximian.com>
2006-08-20 13:53:32 +0000

    adopt vbnc batch files to their new location at mono-basic
    svn path=/trunk/mono-basic/; revision=64101

--- vbruntime/vbnc.tests.build.bat
+++ vbruntime/vbnc.tests.build.bat
@@ -1,10 +1,28 @@
-echo on
-IF NOT DEFINED VSINSTALLDIR echo 'please set VS vars!'
-echo running tests using .NET %FRAMEWORKVERSION%
+echo ====================================
+echo = 	Batch build VisualBasic vbnc tests
+echo =	
+echo =	sample usage: build VB vbnc tests using .NET 1.1
+echo =	vbnc.tests.build.bat 1
+echo =	
+echo =	sample use: build VB vbnc tests using .NET 2.0
+echo =	vbnc.tests.build.bat 2
+echo =  
+echo ====================================
+echo off
+echo Get batch command parameters.
+SET VB_BUILD_PARAM_NET_VERSION="%1"
 
-mkdir build
-pushd build
+echo Set command parameters default.
+IF %VB_BUILD_PARAM_NET_VERSION%=="" SET VB_BUILD_PARAM_NET_VERSION="2"
+
+echo Set .NET SDK env.
+IF %VB_BUILD_PARAM_NET_VERSION%=="1" (
+IF NOT DEFINED VSINSTALLDIR call "%VS71COMNTOOLS%vsvars32.bat"
+)
+IF %VB_BUILD_PARAM_NET_VERSION%=="2" (
+IF NOT DEFINED VSINSTALLDIR call "%VS80COMNTOOLS%vsvars32.bat"
+)
+
+echo on
 DIR ..\..\vbnc\vbnc\tests\*.vb /s /b >vbnc.tests.build.files.tmp
 for /f "delims=;" %%f in (vbnc.tests.build.files.tmp) do vbc -target:exe -out:"%%~pf%%~nf.exe" -debug:full -r:mscorlib.dll -r:System.dll -r:Microsoft.VisualBasic.dll "%%f"
-
-popd
--- vbruntime/vbnc.tests.run.bat
+++ vbruntime/vbnc.tests.run.bat
@@ -1,5 +1,3 @@
 echo on
-pushd build
 DIR ..\..\vbnc\vbnc\tests\*.exe /s /b >vbnc.tests.run.files.tmp
 for /f "delims=;" %%f in (vbnc.tests.run.files.tmp) do "%%~pf%%~nf" >"%%~pf%%~nf.log" 2>&1
-popd
