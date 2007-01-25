7a85c83d97f9299dd1242856c33bc8a233c0d2dd
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-01-25 08:36:22 +0000

    2007-01-25  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* Updated and fixed the batch build files for new source files.
    
    svn path=/trunk/mono-basic/; revision=71652

--- vbruntime/VB.build.bat
+++ vbruntime/VB.build.bat
@@ -29,105 +29,37 @@ IF NOT DEFINED VSINSTALLDIR call "%VS80COMNTOOLS%vsvars32.bat"
 )
 
 echo Set VB compile options.
-SET VB_COMPILE_OPTIONS=
-IF %VB_BUILD_PARAM_NET_VERSION%=="1" ( 
-IF NOT %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=/define:DEBUG=False
-IF %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=/debug:full /define:DEBUG=True,TRACE=True 
-)
-
 rem The option /errorreport:prompt is used to alert the vbc compiler to prompt the reason of a failure.
 
-IF %VB_BUILD_PARAM_NET_VERSION%=="2" (
 SET VB_COMPILE_OPTIONS=
-SET VB_COMPILE_OPTIONS=/nowarn:42016,41999,42017,42018,42019,42032,42036,42020,42021,42022,40005 /errorreport:prompt /noconfig 
+IF %VB_BUILD_PARAM_NET_VERSION%=="1" ( 
+GOTO SETOPTIONS1
+)
+IF %VB_BUILD_PARAM_NET_VERSION%=="2" (
+GOTO SETOPTIONS2
+)
+GOTO ENDSETOPTIONS
+
+:SETOPTIONS2
+SET VB_COMPILE_OPTIONS=/nowarn:42016,41999,42017,42018,42019,42032,42036,42020,42021,42022,40005 /errorreport:prompt /noconfig /imports:System.Collections,System.Diagnostics,System.Collections.Generic,System
 IF NOT %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=%VB_COMPILE_OPTIONS% /define:DEBUG=False,NET_2_0=True,_MYTYPE=\"Empty\"
 IF %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=%VB_COMPILE_OPTIONS% /debug:full /define:DEBUG=True,TRACE=False,NET_2_0=True,_MYTYPE=\"Empty\" /errorreport:prompt  -verbose
-)
+GOTO ENDSETOPTIONS
+
+:SETOPTIONS1
+IF NOT %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=/define:DEBUG=False
+IF %VB_BUILD_PARAM_CONFIGURATION%=="debug" SET VB_COMPILE_OPTIONS=/debug:full /define:DEBUG=True,TRACE=True 
+GOTO ENDSETOPTIONS
+
 echo %VB_COMPILE_OPTIONS%
+
+:ENDSETOPTIONS
 
 echo Set VB compile references
 SET VB_COMPILE_REFERENCES=
 SET VB_COMPILE_REFERENCES=-r:mscorlib.dll -r:System.dll -r:System.Windows.Forms.dll -r:System.Drawing.dll %VB_COMPILE_REFERENCES_J2EE%
 
-SET VB_SOURCES=AssemblyInfo.vb ^
-Microsoft.VisualBasic\AppWinStyle.vb ^
-Microsoft.VisualBasic\AudioPlayMode.vb ^
-Microsoft.VisualBasic\CallType.vb ^
-Microsoft.VisualBasic\Collection.vb ^
-Microsoft.VisualBasic\ComClassAttribute.vb ^
-Microsoft.VisualBasic\CompareMethod.vb ^
-Microsoft.VisualBasic\Constants.vb ^
-Microsoft.VisualBasic\ControlChars.vb ^
-Microsoft.VisualBasic\Conversion.vb ^
-Microsoft.VisualBasic\DateAndTime.vb ^
-Microsoft.VisualBasic\DateFormat.vb ^
-Microsoft.VisualBasic\DateInterval.vb ^
-Microsoft.VisualBasic\DueDate.vb ^
-Microsoft.VisualBasic\ErrObject.vb ^
-Microsoft.VisualBasic\FileAttribute.vb ^
-Microsoft.VisualBasic\FileSystem.vb ^
-Microsoft.VisualBasic\Financial.vb ^
-Microsoft.VisualBasic\HideModuleNameAttribute.vb ^
-Microsoft.VisualBasic\Information.vb ^
-Microsoft.VisualBasic\Interaction.vb ^
-Microsoft.VisualBasic\FirstDayOfWeek.vb ^
-Microsoft.VisualBasic\FirstWeekOfYear.vb ^
-Microsoft.VisualBasic\MsgBoxResult.vb ^
-Microsoft.VisualBasic\MsgBoxStyle.vb ^
-Microsoft.VisualBasic\MyGroupCollectionAttribute.vb ^
-Microsoft.VisualBasic\OpenAccess.vb ^
-Microsoft.VisualBasic\OpenMode.vb ^
-Microsoft.VisualBasic\OpenShare.vb ^
-Microsoft.VisualBasic\SpcInfo.vb ^
-Microsoft.VisualBasic\Strings.vb ^
-Microsoft.VisualBasic\TabInfo.vb ^
-Microsoft.VisualBasic\TriState.vb ^
-Microsoft.VisualBasic\VariantType.vb ^
-Microsoft.VisualBasic\VBFixedArrayAttribute.vb ^
-Microsoft.VisualBasic\VBFixedStringAttribute.vb ^
-Microsoft.VisualBasic\VBMath.vb ^
-Microsoft.VisualBasic\VbStrConv.vb ^
-Microsoft.VisualBasic.CompilerServices\BooleanType.vb ^
-Microsoft.VisualBasic.CompilerServices\ByteType.vb ^
-Microsoft.VisualBasic.CompilerServices\CharArrayType.vb ^
-Microsoft.VisualBasic.CompilerServices\CharType.vb ^
-Microsoft.VisualBasic.CompilerServices\Conversions.vb ^
-Microsoft.VisualBasic.CompilerServices\DateType.vb ^
-Microsoft.VisualBasic.CompilerServices\DecimalType.vb ^
-Microsoft.VisualBasic.CompilerServices\DesignerGeneratedAttribute.vb ^
-Microsoft.VisualBasic.CompilerServices\DoubleType.vb ^
-Microsoft.VisualBasic.CompilerServices\ExceptionUtils.vb ^
-Microsoft.VisualBasic.CompilerServices\FlowControl.vb ^
-Microsoft.VisualBasic.CompilerServices\HostServices.vb ^
-Microsoft.VisualBasic.CompilerServices\IncompleteInitialization.vb ^
-Microsoft.VisualBasic.CompilerServices\IntegerType.vb ^
-Microsoft.VisualBasic.CompilerServices\InternalErrorException.vb ^
-Microsoft.VisualBasic.CompilerServices\IVbHost.vb ^
-Microsoft.VisualBasic.CompilerServices\LateBinder.vb ^
-Microsoft.VisualBasic.CompilerServices\LateBinding.vb ^
-Microsoft.VisualBasic.CompilerServices\LikeOperator.vb ^
-Microsoft.VisualBasic.CompilerServices\LongType.vb ^
-Microsoft.VisualBasic.CompilerServices\NewLateBinding.vb ^
-Microsoft.VisualBasic.CompilerServices\ObjectFlowControl.vb ^
-Microsoft.VisualBasic.CompilerServices\ObjectType.vb ^
-Microsoft.VisualBasic.CompilerServices\Operators.vb ^
-Microsoft.VisualBasic.CompilerServices\OptionCompareAttribute.vb ^
-Microsoft.VisualBasic.CompilerServices\OptionTextAttribute.vb ^
-Microsoft.VisualBasic.CompilerServices\ProjectData.vb ^
-Microsoft.VisualBasic.CompilerServices\ShortType.vb ^
-Microsoft.VisualBasic.CompilerServices\SingleType.vb ^
-Microsoft.VisualBasic.CompilerServices\StandardModuleAttribute.vb ^
-Microsoft.VisualBasic.CompilerServices\StaticLocalInitFlag.vb ^
-Microsoft.VisualBasic.CompilerServices\StringType.vb ^
-Microsoft.VisualBasic.CompilerServices\Utils.vb ^
-Microsoft.VisualBasic.CompilerServices\Versioned.vb ^
-Microsoft.VisualBasic.ApplicationServices\ApplicationBase.vb ^
-Microsoft.VisualBasic.ApplicationServices\AuthenticationMode.vb ^
-Microsoft.VisualBasic.ApplicationServices\ConsoleApplicationBase.vb ^
-Microsoft.VisualBasic.ApplicationServices\ShutdownMode.vb ^
-Microsoft.VisualBasic.ApplicationServices\User.vb ^
-Microsoft.VisualBasic.ApplicationServices\WindowsFormsApplicationBase.vb ^
-Microsoft.VisualBasic.Devices\Computer.vb
+SET VB_SOURCES=@Microsoft.VisualBasic.dll.sources.win
 
 echo Set log file options.
 set startDate=%date%
