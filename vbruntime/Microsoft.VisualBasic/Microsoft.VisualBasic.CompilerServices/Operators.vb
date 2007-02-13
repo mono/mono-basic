e09754a5881abee1839f61bc53d5cb6e85d36543
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-02-13 11:56:44 +0000

    * Operators.vb: Implemented CompareObjectEqual partially. * TypeCombinations.vb: Added a enum of all combinations of TypeCodes.
    svn path=/trunk/mono-basic/; revision=72754

--- vbruntime/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/Operators.vb
+++ vbruntime/Microsoft.VisualBasic/Microsoft.VisualBasic.CompilerServices/Operators.vb
@@ -32,6 +32,132 @@
 Imports System
 Namespace Microsoft.VisualBasic.CompilerServices
     Public Class Operators
+        Private Shared Function CompareBoolean(ByVal Left As Boolean, ByVal Right As Boolean) As Integer
+            If Left = Right Then Return 0
+            If Left < Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareByte(ByVal Left As Byte, ByVal Right As Byte) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareChar(ByVal Left As Char, ByVal Right As Char) As Integer
+            If Left = Right Then Return 0
+            If Left < Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareDate(ByVal Left As Date, ByVal Right As Date) As Integer
+            Return DateTime.Compare(Left, Right)
+        End Function
+
+        Private Shared Function CompareDecimal(ByVal Left As Decimal, ByVal Right As Decimal) As Integer
+            Return Decimal.Compare(Left, Right)
+        End Function
+
+        Private Shared Function CompareDouble(ByVal Left As Double, ByVal Right As Double) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareInt16(ByVal Left As Int16, ByVal Right As Int16) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareInt32(ByVal Left As Int32, ByVal Right As Int32) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareInt64(ByVal Left As Int64, ByVal Right As Int64) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareSByte(ByVal Left As SByte, ByVal Right As SByte) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareSingle(ByVal Left As Single, ByVal Right As Single) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareUInt16(ByVal Left As UInt16, ByVal Right As UInt16) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareUInt32(ByVal Left As UInt32, ByVal Right As UInt32) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
+        Private Shared Function CompareUInt64(ByVal Left As UInt64, ByVal Right As UInt64) As Integer
+            If Left = Right Then
+                Return 0
+            ElseIf Left > Right Then
+                Return 1
+            Else
+                Return -1
+            End If
+        End Function
+
         Public Shared Function AddObject(ByVal Left As Object, ByVal Right As Object) As Object
             Throw New NotImplementedException
         End Function
@@ -42,6 +168,66 @@ Namespace Microsoft.VisualBasic.CompilerServices
             Throw New NotImplementedException
         End Function
         Public Shared Function CompareObjectEqual(ByVal Left As Object, ByVal Right As Object, ByVal TextCompare As Boolean) As Object
+            Dim codeLeft, codeRight As TypeCode
+            Const codeNothing As TypeCode = TypeCode.Empty
+
+            If Left Is Nothing Then
+                codeLeft = codeNothing
+            Else
+                codeLeft = Type.GetTypeCode(Left.GetType)
+            End If
+            If Right Is Nothing Then
+                codeRight = codeNothing
+            Else
+                codeRight = Type.GetTypeCode(Right.GetType)
+            End If
+
+            If codeRight = codeNothing AndAlso codeLeft = codeNothing Then Return 0
+
+            If codeRight = codeLeft Then
+                Select Case codeLeft
+                    Case TypeCode.Boolean
+                        Return CompareBoolean(CBool(Left), CBool(Right))
+                    Case TypeCode.Byte
+                        Return CompareByte(CByte(Left), CByte(Right))
+                    Case TypeCode.Char
+                        Return CompareChar(CChar(Left), CChar(Right))
+                    Case TypeCode.DateTime
+                        Return CompareDate(CDate(Left), CDate(Right))
+                    Case TypeCode.Decimal
+                        Return CompareDecimal(CDec(Left), CDec(Right))
+                    Case TypeCode.Double
+                        Return CompareDouble(CDbl(Left), CDbl(Right))
+                    Case TypeCode.Int16
+                        Return CompareInt16(CShort(Left), CShort(Right))
+                    Case TypeCode.Int32
+                        Return CompareInt32(CInt(Left), CInt(Right))
+                    Case TypeCode.Int64
+                        Return CompareInt64(CLng(Left), CLng(Right))
+                    Case TypeCode.SByte
+                        Return CompareSByte(CSByte(Left), CSByte(Right))
+                    Case TypeCode.Single
+                        Return CompareSingle(CSng(Left), CSng(Right))
+                    Case TypeCode.String
+                        Return CompareString(CStr(Left), CStr(Right), TextCompare)
+                    Case TypeCode.UInt16
+                        Return CompareUInt16(CUShort(Left), CUShort(Right))
+                    Case TypeCode.UInt32
+                        Return CompareUInt32(CUInt(Left), CUInt(Right))
+                    Case TypeCode.UInt64
+                        Return CompareUInt64(CULng(Left), CULng(Right))
+                    Case TypeCode.Object
+                        Throw New NotImplementedException
+                    Case TypeCode.DBNull
+                        Throw New NotImplementedException
+                End Select
+            End If
+
+            Select Case CType(codeLeft << TypeCombinations.SHIFT Or codeRight, TypeCombinations)
+                Case TypeCombinations.Boolean_String
+                    Return CompareBoolean(CBool(Left), Conversions.ToBoolean(Right))
+            End Select
+
             Throw New NotImplementedException
         End Function
         Public Shared Function CompareObjectGreater(ByVal Left As Object, ByVal Right As Object, ByVal TextCompare As Boolean) As Object
