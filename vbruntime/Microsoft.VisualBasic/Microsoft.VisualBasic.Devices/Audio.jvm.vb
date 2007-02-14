37b9b684d458a0a618a74f0c53f8553d314a79d8
Boris Kirzner <borisk@mono-cvs.ximian.com>
2007-02-14 15:26:01 +0000

    Added stub jvm.vb implementations for basic VB console application.
    svn path=/trunk/mono-basic/; revision=72839

---
+++ vbruntime/Microsoft.VisualBasic/Microsoft.VisualBasic.Devices/Audio.jvm.vb
@@ -0,0 +1,71 @@
+'
+' Audio.vb
+'
+' Authors:
+'   Boris Kirzner (borisk@mainsoft.com)
+'
+' Copyright (C) 2007 Novell (http://www.novell.com)
+'
+' Permission is hereby granted, free of charge, to any person obtaining
+' a copy of this software and associated documentation files (the
+' "Software"), to deal in the Software without restriction, including
+' without limitation the rights to use, copy, modify, merge, publish,
+' distribute, sublicense, and/or sell copies of the Software, and to
+' permit persons to whom the Software is furnished to do so, subject to
+' the following conditions:
+' 
+' The above copyright notice and this permission notice shall be
+' included in all copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+' EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+' NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+' LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+' OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+' WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+#If NET_2_0 Then
+#If TARGET_JVM = True Then
+Imports System
+Imports System.IO
+Imports System.Media
+
+Namespace Microsoft.VisualBasic.Devices
+    Public Class Audio
+
+        Public Sub New()
+            Throw New NotImplementedException
+        End Sub
+
+        Public Sub Play(ByVal location As String)
+            Throw New NotImplementedException
+        End Sub
+
+        'Public Sub Play(ByVal location As String, ByVal playMode As AudioPlayMode)
+        '    Throw New NotImplementedException
+        'End Sub
+
+        'Public Sub Play(ByVal stream As Stream, ByVal playMode As AudioPlayMode)
+        '    Throw New NotImplementedException
+        'End Sub
+
+        'Public Sub Play(ByVal data As Byte(), ByVal playMode As AudioPlayMode)
+        '    Throw New NotImplementedException
+        'End Sub
+
+        'Public Sub PlaySystemSound(ByVal systemSound As SystemSound)
+        '    Throw New NotImplementedException
+        'End Sub
+
+        'Public Sub [Stop]()
+        '    Throw New NotImplementedException
+        'End Sub
+
+        'Private Sub Play(ByVal player As SoundPlayer, ByVal mode As AudioPlayMode)
+        '    Throw New NotImplementedException
+        'End Sub
+    End Class
+End Namespace
+#End If
+#End If
---
+++ vbruntime/Microsoft.VisualBasic/Microsoft.VisualBasic.Devices/Computer.jvm.vb
@@ -0,0 +1,82 @@
+'
+' Computer.jvm.vb
+'
+' Authors:
+'   Boris Kirzner (borisk@mainsoft.com)
+'
+'
+' Copyright (C) 2002-2006 Mainsoft Corporation.
+' Copyright (C) 2006-2007 Novell (http://www.novell.com)
+'
+' Permission is hereby granted, free of charge, to any person obtaining
+' a copy of this software and associated documentation files (the
+' "Software"), to deal in the Software without restriction, including
+' without limitation the rights to use, copy, modify, merge, publish,
+' distribute, sublicense, and/or sell copies of the Software, and to
+' permit persons to whom the Software is furnished to do so, subject to
+' the following conditions:
+' 
+' The above copyright notice and this permission notice shall be
+' included in all copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+' EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+' NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+' LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+' OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+' WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+ 
+#If NET_2_0 Then
+#If TARGET_JVM = True Then
+Imports System
+Imports Microsoft.VisualBasic.MyServices
+
+Namespace Microsoft.VisualBasic.Devices
+    Public Class Computer
+        Inherits ServerComputer
+
+        Public Sub New()
+            Throw New NotImplementedException
+        End Sub
+
+        Public ReadOnly Property Audio() As Audio
+            Get
+                Throw New NotImplementedException
+            End Get
+        End Property
+
+        Public ReadOnly Property Clipboard() As MyServices.ClipboardProxy
+            Get
+                Throw New NotImplementedException
+            End Get
+        End Property
+
+        Public ReadOnly Property Keyboard() As Keyboard
+            Get
+                Throw New NotImplementedException
+            End Get
+        End Property
+
+        Public ReadOnly Property Mouse() As Mouse
+            Get
+                Throw New NotImplementedException
+            End Get
+        End Property
+
+        Public ReadOnly Property Ports() As Ports
+            Get
+                Throw New NotImplementedException
+            End Get
+        End Property
+
+        'Public ReadOnly Property Screen() As Screen
+        '    Get
+        '        Throw New NotImplementedException
+        '    End Get
+        'End Property
+    End Class
+End Namespace
+#End If
+#End If
