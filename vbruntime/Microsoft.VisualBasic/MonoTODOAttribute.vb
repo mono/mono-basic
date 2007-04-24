c8215dee2d2c635f16cbd92bacd8132a1c68e545
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-04-24 09:18:09 +0000

    * MonoTODOAttribute.vb: Build fix.
    svn path=/trunk/mono-basic/; revision=76169

--- vbruntime/Microsoft.VisualBasic/MonoTODOAttribute.vb
+++ vbruntime/Microsoft.VisualBasic/MonoTODOAttribute.vb
@@ -31,7 +31,7 @@
 ' OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 ' WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '
-
+Imports System
 'Namespace System
 
 <AttributeUsage(AttributeTargets.All)> _
