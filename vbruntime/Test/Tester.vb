3054842134c315305d0632d9ed76fce0310739a5
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-05-09 17:10:42 +0000

    * 2005VB.sln: Update.
    svn path=/trunk/mono-basic/; revision=77046

---
+++ vbruntime/Test/Tester.vb
@@ -0,0 +1,10 @@
+Module Tester
+
+    Function Main() As Integer
+        Dim t As New MonoTests.Microsoft_VisualBasic.CompilerServices.UtilsTests
+ 
+        t.ReDimPreserve_MultipleDimensions()
+
+    End Function
+
+End Module
