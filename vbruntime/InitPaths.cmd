3a203b1f576832ce61c958ccd1c319313e5c7f34
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-05-10 11:40:19 +0000

    Missing file.
    svn path=/trunk/mono-basic/; revision=77122

---
+++ vbruntime/InitPaths.cmd
@@ -0,0 +1,44 @@
+ECHO OFF
+IF NOT "%1"=="" (SET FXVERSION=%1)
+IF "%FXVERSION%"=="" (
+	ECHO No fx version specified
+	ECHO Set FXVERSION to either 1_0, 1_1, 1, 2_0 or 2, or pass the version as a parameter
+	SET ERRORLEVEL=1
+	GOTO EOF
+)
+
+
+SET FXPATH_1_0=%WINDIR%\Microsoft.NET\Framework\v1.0.3705
+SET FXPATH_1_1=%WINDIR%\Microsoft.NET\Framework\v1.1.4322
+SET FXPATH_2_0=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
+
+SET CSC_1_0=%FXPATH_1_0%\csc.exe
+SET CSC_1_1=%FXPATH_1_1%\csc.exe
+SET CSC_2_0=%FXPATH_2_0%\csc.exe
+
+SET VBC_1_0=%FXPATH_1_0%\vbc.exe
+SET VBC_1_1=%FXPATH_1_1%\vbc.exe
+SET VBC_2_0=%FXPATH_2_0%\vbc.exe
+
+IF NOT EXIST %VBC_1_0% (SET VBC_1_0=%VBC_1_1%)
+IF NOT EXIST %CSC_1_0% (SET CSC_1_0=%CSC_1_1%)
+
+IF "%FXVERSION%"=="1_0" (SET VBC=%VBC_1_0%)
+IF "%FXVERSION%"=="1_1" (SET VBC=%VBC_1_1%)
+IF "%FXVERSION%"=="1" (SET VBC=%VBC_1_1%)
+IF "%FXVERSION%"=="2" (SET VBC=%VBC_2_0%)
+IF "%FXVERSION%"=="2_0" (SET VBC=%VBC_2_0%)
+
+IF "%FXVERSION%"=="1_0" (SET CSC=%CSC_1_0%)
+IF "%FXVERSION%"=="1_1" (SET CSC=%CSC_1_1%)
+IF "%FXVERSION%"=="1" (SET CSC=%CSC_1_1%)
+IF "%FXVERSION%"=="2" (SET CSC=%CSC_2_0%)
+IF "%FXVERSION%"=="2_0" (SET CSC=%CSC_2_0%)
+
+
+ECHO.
+ECHO Paths found (for FXVERSION=%FXVERSION%):
+ECHO VBC compiler: %VBC%
+ECHO CSC compiler: %CSC%
+ECHO.
+:EOF
