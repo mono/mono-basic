ff8aa53075ca29a2477f746cfd8582d998af74ed
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-05-10 11:35:51 +0000

    * Make.cmd: Update, can now build both 1.0 and 2.0. * Makefile: Update to use either mcs or gmcs according to target   version.
    svn path=/trunk/mono-basic/; revision=77120

--- vbruntime/Test/Make.cmd
+++ vbruntime/Test/Make.cmd
@@ -1,50 +1,70 @@
+ECHO OFF
+cls
+pushd .
+SETLOCAL
 
+IF "%FXVERSION%"=="" (SET FXVERSION=%1)
 
-SET PATH1=%WINDIR%\Microsoft.NET\Framework\v1.0.3705
-SET PATH1_1=%WINDIR%\Microsoft.NET\Framework\v1.1.4322
-SET PATH2=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
-
-SET CSC1=%PATH1%\csc.exe
-SET CSC1_1=%PATH1_1%\csc.exe
-SET CSC2=%PATH2%\csc.exe
-
-IF EXIST %CSC1% (SET CSC=%CSC1%)
-IF EXIST %CSC1_1% (SET CSC=%CSC1_1%)
-IF EXIST %CSC2% (SET CSC=%CSC2%)
-
+CALL ..\InitPaths.cmd 
+rem ECHO ON
+IF NOT "%ERRORLEVEL%"=="0" GOTO ENDOFFILE
 
 rem 
 rem Build the vbruntime first without signing it, this way it will be linked to the test assembly and we're executing against our vbruntime
 rem Then build it signed, so that we can test against the MS vbruntime.
 rem 
-pushd .
 cd ..
-SET VB_EXTERNAL_OPTIONS=/define:DONTSIGN=true
-CALL VB.build.bat 2 debug
-copy bin\Microsoft.VisualBasic.dll Test\Microsoft.VisualBasic.NOTSIGNED.dll
-SET VB_EXTERNAL_OPTIONS=
-CALL VB.build.bat 2 debug
-copy bin\Microsoft.VisualBasic.dll Test\Microsoft.VisualBasic.SIGNED.dll
+SET EXTRA_VBRUNTIME_FLAGS=/define:DONTSIGN=true
+CALL Make.cmd
+IF NOT "%COMPILATIONERROR%"=="" GOTO ENDOFFILE
+copy Microsoft.VisualBasic\Microsoft.VisualBasic.dll Test\Microsoft.VisualBasic.NOTSIGNED.dll
+SET EXTRA_VBRUNTIME_FLAGS=
+CALL Make.cmd
+IF NOT "%COMPILATIONERROR%"=="" GOTO ENDOFFILE
+copy Microsoft.VisualBasic\Microsoft.VisualBasic.dll Test\Microsoft.VisualBasic.SIGNED.dll
 popd
+
+IF %ERRORLEVEL%==1 GOTO ENDOFFILE
 
 
 copy Microsoft.VisualBasic.SIGNED.dll Microsoft.VisualBasic.dll
 CALL :EXECUTETEST
+IF NOT "%ERRORLEVEL%"=="0" GOTO ENDOFFILE
 copy Microsoft.VisualBasic.NOTSIGNED.dll Microsoft.VisualBasic.dll
 CALL :EXECUTETEST
-del Microsoft.VisualBasic.*.dll
-del Microsoft.VisualBasic.dll
+rem del Microsoft.VisualBasic.*.dll
+rem del Microsoft.VisualBasic.dll
 
 GOTO ENDOFFILE
 
 :EXECUTETEST
 pushd .
 copy Microsoft.VisualBasic.dll bin
-%CSC% "-out:bin\2005VB_test_CS.dll" @2005VB_test_CS.dll.rsp @2005VB_test_CS.dll.sources.win -lib:bin -define:NET_1_0,NET_1_1,NET_2_0
+IF "%FXVERSION%"=="2" (
+	SET CSDEFINES=-define:NET_1_0,NET_1_1,NET_2_0
+) ELSE (
+	SET CSDEFINES=-define:NET_1_0,NET_1_1 -debug-
+)
+%CSC% "-out:bin\2005VB_test_CS.dll" @2005VB_test_CS.dll.rsp @2005VB_test_CS.dll.sources.win -lib:bin %CSDEFINES%
+
 IF ERRORLEVEL 1 (GOTO ENDOFFILE)
 cd bin
-nunit-console.exe 2005VB_test_CS.dll /labels /noshadow /exclude:Slow
+
+IF "%FXVERSION%"=="2" GOTO SETNUNIT2
+IF "%FXVERSION%"=="2_0" GOTO SETNUNIT2
+GOTO SETNUNIT1
+:SETNUNIT1
+copy nunit-console1.exe.config nunit-console.exe.config
+GOTO ENDSETNUNITV
+:SETNUNIT2
+copy nunit-console2.exe.config nunit-console.exe.config
+GOTO ENDSETNUNITV
+:ENDSETNUNITV
+
+nunit-console.exe 2005VB_test_CS.dll /labels /noshadow /exclude:Slow,NotDotNet
+ECHO %ERRORLEVEL%
+IF NOT "%ERRORLEVEL%"=="0" GOTO ENDOFFILE
 rem del Microsoft.VisualBasic.dll
-popd .
 
 :ENDOFFILE
+popd
