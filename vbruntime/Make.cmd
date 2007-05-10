95c77c76125ecaeba0ae12b5de1dc32e0f7d6528
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-05-10 11:39:51 +0000

    Added nunit-console.exe.config files for both 1.0 and 2.0 profile. svn prop updates.
    svn path=/trunk/mono-basic/; revision=77121

--- vbruntime/Make.cmd
+++ vbruntime/Make.cmd
@@ -1,10 +1,21 @@
-SET BASEDIR=%~dp0..\
-SETLOCAL
 
-CALL %BASEDIR%tools\win-make\Make.cmd
-IF ERRORLEVEL 1 GOTO ENDFILE
-%BASEDIR%tools\win-make\win-make.exe %*
-goto ENDFILE
+IF "%FXVERSION%"=="" (SET FXVERSION=%1)
 
-:ENDFILE
-ENDLOCAL
+CALL InitPaths.cmd
+ECHO ON
+IF NOT "%ERRORLEVEL%"=="0" GOTO EOF
+
+
+pushd .
+
+SET RSPFILE=1
+IF "%FXVERSION%"=="2" (SET RSPFILE=2)
+IF "%FXVERSION%"=="2_0" (SET RSPFILE=2)
+
+CD Microsoft.VisualBasic
+%VBC% @vbruntime%RSPFILE%.rsp @Microsoft.VisualBasic.dll.sources.win %EXTRA_VBRUNTIME_FLAGS%
+IF NOT "%ERRORLEVEL%"=="0" SET COMPILATIONERROR="%ERRORLEVEL%"
+popd
+
+
+:EOF
