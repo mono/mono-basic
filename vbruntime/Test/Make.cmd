912ea0151c2cdfee12c74485479eb4d9b6c0a19e
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-05-30 18:37:06 +0000

    * Make.cmd: Exclude NotWorking tests and set correct defines according   to fx version. * Makefile: Updated, copy mdb files to output directory with assembly,   and exclude NotWorking tests.
    svn path=/trunk/mono-basic/; revision=78250

--- vbruntime/Test/Make.cmd
+++ vbruntime/Test/Make.cmd
@@ -40,11 +40,24 @@ GOTO ENDOFFILE
 :EXECUTETEST
 pushd .
 copy Microsoft.VisualBasic.dll bin
-IF "%FXVERSION%"=="2" (
+
+IF "%FXVERSION%"=="2"   (GOTO SETDEFINES2_0)
+IF "%FXVERSION%"=="2_0" (GOTO SETDEFINES2_0)
+IF "%FXVERSION%"=="1_1" (GOTO SETDEFINES1_1)
+IF "%FXVERSION%"=="1"   (GOTO SETDEFINES1_1)
+IF "%FXVERSION%"=="1_0" (GOTO SETDEFINES1_0)
+GOTO ENDSETDEFINES
+:SETDEFINES2_0
 	SET CSDEFINES=-define:NET_1_0,NET_1_1,NET_2_0
-) ELSE (
+	GOTO ENDSETDEFINES
+:SETDEFINES1_1
 	SET CSDEFINES=-define:NET_1_0,NET_1_1 -debug-
-)
+	GOTO ENDSETDEFINES
+:SETDEFINES1_0
+	SET CSDEFINES=-define:NET_1_0 -debug-
+	GOTO ENDSETDEFINES
+:ENDSETDEFINES
+
 %CSC% "-out:bin\2005VB_test_CS.dll" @2005VB_test_CS.dll.rsp @2005VB_test_CS.dll.sources.win -lib:bin %CSDEFINES%
 
 IF ERRORLEVEL 1 (GOTO ENDOFFILE)
@@ -61,7 +74,7 @@ copy nunit-console2.exe.config nunit-console.exe.config
 GOTO ENDSETNUNITV
 :ENDSETNUNITV
 
-nunit-console.exe 2005VB_test_CS.dll /labels /noshadow /exclude:Slow,NotDotNet
+nunit-console.exe 2005VB_test_CS.dll /labels /noshadow /exclude:Slow,NotDotNet,NotWorking
 ECHO %ERRORLEVEL%
 IF NOT "%ERRORLEVEL%"=="0" GOTO ENDOFFILE
 rem del Microsoft.VisualBasic.dll
