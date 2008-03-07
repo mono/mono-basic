6432fc1eb34bfb5886d1433feb73fdbc742a58ee
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-03-07 00:12:44 +0000

    * tests/Bugs/bug-325331.vb: Added.
    svn path=/trunk/mono-basic/; revision=97647

---
+++ vbnc/vbnc/tests/Bugs/bug-325331.vb
@@ -0,0 +1,32 @@
+﻿Class HashDefine
+
+    Sub Two()
+#If FOO Then
+        this is any old nonsense here
+#Else
+        Console.WriteLine("aaaa")
+#End If
+    End Sub
+
+    Sub Three()
+#If FOO Then
+        myObj.Bar("this is any old nonsense here");
+        myObj2.Foo("broken by semicolons, and question marks")?
+#Else
+        myObj.Foo("aaaa")
+#End If
+    End Sub
+
+    Private myObj, myObj2
+
+    Shared Sub Main()
+        Console.WriteLine("before")
+#If FOO Then
+        Console.WriteLine("is")
+#Else
+        Console.WriteLine("not")
+#End If
+        Console.WriteLine("after")
+    End Sub
+
+End Class
