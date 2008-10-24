d0fcaa04423705651488926180595091fd233a33
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-10-24 15:09:14 +0000

    * vbnc/tests/Bugs/bug-438683.vb,   vbnc/source/Parser/Parser(Expressions).vb: Parser: fix parsing of   Not expressions followed by AndAlso/OrElse expressions.
    svn path=/trunk/mono-basic/; revision=116956

---
+++ vbnc/vbnc/tests/Bugs/bug-438683.vb
@@ -0,0 +1,19 @@
+﻿Imports system
+Class C
+    Shared Function main() As Integer
+        Dim s As String = Nothing
+        Dim result As Boolean
+
+        result = Not s Is Nothing AndAlso False
+
+        result = Not result
+
+        If result Then
+            Console.writeline("SUCCESS")
+            Return 0
+        Else
+            Console.writeline("FAIL")
+            Return 1
+        End If
+    End Function
+End Class
