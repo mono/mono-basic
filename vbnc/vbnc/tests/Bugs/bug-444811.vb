0e65dae2b8a2eb38ced1e0924842e97e24a0032b
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-11-17 12:15:33 +0000

    * source/Members/ExternalSubDeclaration.vb, tests/Bugs/bug-444811.vb:   Properly handle external method declarations which doesn't specify   neither Ansi, Unicode nor Auto. Fixes #444811.
    svn path=/trunk/mono-basic/; revision=118999

---
+++ vbnc/vbnc/tests/Bugs/bug-444811.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Class bug_444811
+    Declare Function GetClassName Lib "user32" Alias "GetClassNameA" (ByVal ByValhWnd As IntPtr, ByVal lpClassName As String, ByVal nMaxCount As Int32) As Int32
+
+    Shared Sub Main()
+
+    End Sub
+End Class
