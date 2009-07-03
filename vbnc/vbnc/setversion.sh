3263c5f7c9f46f94c0befba9c8b3186a0f6d0a03
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-07-03 21:56:47 +0000

    2009-05-20  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* setversion.sh: Add git support, and fix tarball
    	builds.
    
    svn path=/trunk/mono-basic/; revision=137375

--- vbnc/vbnc/setversion.sh
+++ vbnc/vbnc/setversion.sh
@@ -3,13 +3,22 @@
 VERSION_VB=$1
 VERSION_TMP=version.tmp
 
+SVN_REVISION=`svn info . | grep Revision | sed 's/Revision: //'`
+GIT_REVISION=`git log --no-color --first-parent --pretty=format:%b|grep -m1 git-svn-id|sed -e 's,git-svn-id: \(.*\)@\(.*\) .*,URL: \1 Revision: \2,'|awk -F" " '{print $4}'`
+
 cat ../License\ FileHeader.txt > $VERSION_TMP
 echo "" >> $VERSION_TMP
 echo -n "<Assembly: AssemblyInformationalVersion (\"" >> $VERSION_TMP
 echo -n `more ../../configure | grep VERSION | grep -v echo | grep -v sed | sed 's/VERSION=//'` >> $VERSION_TMP
-echo -n " - r" >> $VERSION_TMP
-echo -n `svn info . | grep Revision | sed 's/Revision: //'` >> $VERSION_TMP
-echo -n "\")>" >> $VERSION_TMP
+echo -n " - " >> $VERSION_TMP
+if [[ "x$SVN_REVISION" != "x" ]]; then
+	echo -n r$SVN_REVISION  >> $VERSION_TMP
+elif [[ "x$GIT_REVISION" != "x" ]]; then
+	echo -n r$GIT_REVISION >> $VERSION_TMP
+else
+	echo -n tarball >> $VERSION_TMP
+fi
+echo "\")>" >> $VERSION_TMP
 
 if ! diff $VERSION_TMP $VERSION_VB ; then
 	cp $VERSION_TMP $VERSION_VB
