8992b8742f2777d8ac838e869ca52cf937f8f5a3
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-06-08 22:47:50 +0000

    Fix copyrights, e-mails and remote executable bits
    svn path=/trunk/mono-basic/; revision=158693

--- vbnc/vbnc/source/General/CommandLine.OptionInferTypes.vb
+++ vbnc/vbnc/source/General/CommandLine.OptionInferTypes.vb
@@ -1,6 +1,6 @@
 ﻿' 
 ' Visual Basic.Net Compiler
-' Copyright (C) 2009 Rolf Bjarne Kvinge, RKvinge@novell.com
+' Copyright (C) 2009 - 2010 Rolf Bjarne Kvinge, RKvinge@novell.com
 ' 
 ' This library is free software; you can redistribute it and/or
 ' modify it under the terms of the GNU Lesser General Public
--- vbnc/vbnc/source/Members/BaseList.vb
+++ vbnc/vbnc/source/Members/BaseList.vb
@@ -1,6 +1,6 @@
 ' 
 ' Visual Basic.Net Compiler
-' Copyright (C) 2004 - 2007 Rolf Bjarne Kvinge, RKvinge@novell.com
+' Copyright (C) 2004 - 2010 Rolf Bjarne Kvinge, RKvinge@novell.com
 ' 
 ' This library is free software; you can redistribute it and/or
 ' modify it under the terms of the GNU Lesser General Public
