403ef2c721119b9131bec426638420d7019343e0
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-06-08 22:50:18 +0000

    2010-06-09  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* General/Version.vb: Updated.
    
    svn path=/trunk/mono-basic/; revision=158713

--- vbnc/vbnc/source/General/Version.vb
+++ vbnc/vbnc/source/General/Version.vb
@@ -16,4 +16,4 @@
 ' License along with this library; if not, write to the Free Software
 ' Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ' 
-<Assembly: AssemblyInformationalVersion ("2.5 - r")>
+<Assembly: AssemblyInformationalVersion ("2.7 - tarball")>
