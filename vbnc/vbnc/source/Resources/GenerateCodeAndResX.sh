341d220e1517fcff99d3c5d2a840a6cde87f8a6e
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-11-05 11:16:35 +0100

    Make the resource generator script automatically compile the msg-generator tool

--- vbnc/vbnc/source/Resources/GenerateCodeAndResX.sh
+++ vbnc/vbnc/source/Resources/GenerateCodeAndResX.sh
@@ -1,4 +1,5 @@
 rm Errors.resx
+make -C ../../../msg-generator
 mono ../../../msg-generator/msg-gen.exe /mergesource:Source.xml  /outvb:../General/Messages.vb /outresx:Errors.resx
 set-lf Errors.resx
 dos2unix ../General/Messages.vb
