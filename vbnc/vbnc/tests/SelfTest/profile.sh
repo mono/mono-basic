fe1a50578620e26c0652e853e3453cae019187a6
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-11-24 22:00:29 +0100

    vbnc: Improved profiler script

--- vbnc/vbnc/tests/SelfTest/profile.sh
+++ vbnc/vbnc/tests/SelfTest/profile.sh
@@ -3,11 +3,16 @@
 make -C ../../../
 cp ../../../../class/lib/vbnc/vbnc.* .
 cp ../../../../class/lib/vbnc/Mono.Cecil* .
+
+# make sure we're using an updated binary
+rerun.sh && rerun.sh
+
+rm output.mlpd
 mono --profile=log:zip vbnc.exe @SelfCompileLinux.response -out:vbnc.profile.exe
 I=1
 while test -e profile$I.log; do
 	let I=I+1
 done
-mprof-report --reports=header,gc,alloc,metadata,exception,monitor,thread,heapshot --traces output.mlpd > profile$I.log
+mprof-report --reports=header,gc,alloc,metadata,exception,monitor,thread,heapshot --alloc-sort=count --traces output.mlpd > profile$I.log
 biiip
 
