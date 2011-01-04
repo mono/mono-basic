00a6e514a9ae8ca660b7fbb776f6e78b158378e6
Rolf Bjarne Kvinge <RKvinge@novell.com>
2011-01-04 14:08:33 +0100

    vbnc: Add a failing type name resolution test

---
+++ vbnc/vbnc/tests/CompileTime/TypeNameResolution3.vb
@@ -0,0 +1,10 @@
+Public Class TypeNameResolution3
+    Public Class MimeType
+        Public Const [Binary] As String = "Binary"
+    End Class
+
+    Shared Sub S(Optional ByVal MimeType As String = MimeType.Binary)
+
+    End Sub
+
+End Class
