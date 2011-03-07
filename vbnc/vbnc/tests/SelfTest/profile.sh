abdedceb7a60e3695398d906ee332b60cf67d8f3
Rolf Bjarne Kvinge <RKvinge@novell.com>
2011-03-07 14:38:18 +0100

    vbnc: Update profiling script after the move to 4.0

--- vbnc/vbnc/tests/SelfTest/profile.sh
+++ vbnc/vbnc/tests/SelfTest/profile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
 make -C ../../../
-cp ../../../../class/lib/vbnc/vbnc.* .
-cp ../../../../class/lib/vbnc/Mono.Cecil* .
+cp ../../../../class/lib/net_4_0/vbnc.* .
+cp ../../../../class/lib/net_4_0/Mono.Cecil* .
 
 # make sure we're using an updated binary
 rerun.sh && rerun.sh
 
-rm output.mlpd
+rm -f output.mlpd
 mono --profile=log:nocalls,zip vbnc.exe @SelfCompileLinux.response -out:vbnc.profile.exe
 I=1
 while test -e profile$I.log; do
