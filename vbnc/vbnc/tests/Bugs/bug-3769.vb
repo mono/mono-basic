82f1153a588bbfbb0f6750ce1cbbba41e5dfa8d3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-07-05 11:12:17 +0200

    Add test case for bug #3769 (dup of #2087)

---
+++ vbnc/vbnc/tests/Bugs/bug-3769.vb
@@ -0,0 +1,12 @@
+Option Explicit On
+Option Strict On
+Option Compare Binary
+REM Option Infer Off
+
+Public Class Test1
+    Public Shared Sub Main()
+        a = 3
+        Dim a As Integer
+        System.Console.WriteLine(a)
+    End Sub
+End Class
